f745479eaeefc25af7bb6d312c1fa88b9933679d
Rishav_Kumar_Mishra <rishavkumarmishra001@gmail.com>
2026-03-26 01:36:26 +0530

    Implement binary tree traversal functions

---
+++ Binary Tree Traversals (Inorder, Preorder, Postorder).c
@@ -0,0 +1,23 @@
+// Inorder
+void inorder(struct Node* root) {
+    if (root == NULL) return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+// Preorder
+void preorder(struct Node* root) {
+    if (root == NULL) return;
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+// Postorder
+void postorder(struct Node* root) {
+    if (root == NULL) return;
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
+}
