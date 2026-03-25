3e4d2f2c3156d347a8ac66d1708979f337d468dd
Rishav_Kumar_Mishra <rishavkumarmishra001@gmail.com>
2026-03-26 01:39:32 +0530

    Create Insert Node in BST.c

---
+++ Insert Node in BST.c
@@ -0,0 +1,10 @@
+struct Node* insert(struct Node* root, int key) {
+    if (root == NULL) return newNode(key);
+
+    if (key < root->data)
+        root->left = insert(root->left, key);
+    else if (key > root->data)
+        root->right = insert(root->right, key);
+
+    return root;
+}
